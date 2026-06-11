51701ba29aa34e2cf5ddc0f2d5f0b1f633176dec
Nithish S <nithi4527@gmail.com>
2026-06-11 23:16:54 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int binarySearch(int[] piles, int h, int upper){
+        int low = 1; int high = upper;
+        int res = high;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            int hrs = 0;
+            for(int i: piles){
+                hrs += (i + mid - 1) / mid;
+            }
+            if(hrs <= h){
+                res = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return res;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int upper = 0;
+        for(int i=0; i<piles.length; i++){
+            if(piles[i] > upper) upper = piles[i];
+        }
+        return binarySearch(piles,h,upper);
+    }
+}
