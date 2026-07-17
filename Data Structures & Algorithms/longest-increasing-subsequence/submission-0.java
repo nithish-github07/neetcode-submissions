ca186ea4066ac3a96edd6eedb618e430c92b33c5
Nithish S <nithi4527@gmail.com>
2026-07-17 22:43:07 +0530

    Add: longest-increasing-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-0.java
@@ -0,0 +1,16 @@
+//Bottom-Up DP Approach
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp,1);
+        for(int i=n-2; i>=0; i--){
+            for(int j=i+1; j<n; j++){
+                if(nums[i] < nums[j]){
+                    dp[i] = Math.max(dp[i],1+dp[j]);
+                }
+            }
+        }
+        return Arrays.stream(dp).max().getAsInt();
+    }
+}
