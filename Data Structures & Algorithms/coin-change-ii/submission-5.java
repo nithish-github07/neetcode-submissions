61efba07383cb005ee9b08b4df4c092cfb0d573f
Nithish S <nithi4527@gmail.com>
2026-07-21 23:06:47 +0530

    Add: coin-change-ii - submission-5

---
+++ Data Structures & Algorithms/coin-change-ii/submission-5.java
@@ -0,0 +1,21 @@
+//O(m*n) - Tabulation
+class Solution {
+    public int change(int amount, int[] coins) {
+        int m = coins.length, n = amount;
+        int[][] dp = new int[m+1][n+1];
+        for(int i=0; i<m; i++){
+            dp[i][0] = 1;
+        }   
+
+        for(int i=m-1; i>=0; i--){
+            for(int j=1; j<=n; j++){
+                dp[i][j] =dp[i+1][j];
+                if(j-coins[i] >= 0){
+                    dp[i][j] +=  dp[i][j-coins[i]];
+                }
+            }
+        }  
+        return dp[0][n];
+    }
+
+}
