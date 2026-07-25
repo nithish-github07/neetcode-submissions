dfeb7820255f03252573b5d7be760c895498c58c
Nithish S <nithi4527@gmail.com>
2026-07-25 23:24:15 +0530

    Add: count-subsequences - submission-2

---
+++ Data Structures & Algorithms/count-subsequences/submission-2.java
@@ -0,0 +1,18 @@
+//tabulation
+class Solution {
+    public int numDistinct(String s, String t) {
+        int m = s.length(), n = t.length();
+        int[][] dp = new int[m+1][n+1];
+        dp[m][n] = 1;
+        for(int i=m-1; i>=0; i--){
+            for(int j=n; j>=0; j--){
+                dp[i][j] = dp[i+1][j];
+                if(j < n && s.charAt(i) == t.charAt(j)){
+                    dp[i][j] += dp[i+1][j+1];
+                }
+            }
+        }
+        return dp[0][0];
+        
+    }
+}
