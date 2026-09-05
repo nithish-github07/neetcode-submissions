ebe02021865c74bd2c5c234e8552c543be1aea88
Nithish S <nithi4527@gmail.com>
2026-09-05 13:54:44 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.java
@@ -0,0 +1,13 @@
+//boyer-moore voting alg
+class Solution {
+    public int majorityElement(int[] nums) {
+        int res = 0, count = 0;
+        for(int num: nums){
+            if(count == 0){
+                res = num;
+            }
+            count += (num == res) ? 1 : -1;
+        }
+        return res;
+    }
+}
