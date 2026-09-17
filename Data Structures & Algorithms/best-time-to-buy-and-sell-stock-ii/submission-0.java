caeca80e0780b4185fd86648cd6a53989c1f4033
Nithish S <nithi4527@gmail.com>
2026-09-17 22:14:52 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int sum = 0;
+        int n = prices.length;
+        for(int i=1; i<n; i++){
+            if(prices[i] > prices[i-1]){
+                sum += prices[i] - prices[i-1];
+            }
+        }
+        return sum;
+    }
+}
